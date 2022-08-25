47ca83eb8b7a1e170172ae03aecf4af0a897835d
JuanRAlvarezP <95868133+JuanRAlvarezP@users.noreply.github.com>
2022-08-25 14:51:46 +0200

    Create Ejercicio2.js

---
+++ JavaScript/02. Listas, obejtos y fechas/Ejercicio2.js
@@ -0,0 +1,13 @@
+const name = "Juan Alvarez";
+const age = 28;
+const isDeveloper = false;
+const birthdate = new Date(1993, 10, 7);
+const favebook = {
+    title: "1984",
+    author: "George Orwell",
+    release: new Date(1949, 5, 8),
+    url: "https://en.wikipedia.org/wiki/Nineteen_Eighty-Four"
+}
+
+const list = [name, age, isDeveloper, birthdate, favebook]
+
