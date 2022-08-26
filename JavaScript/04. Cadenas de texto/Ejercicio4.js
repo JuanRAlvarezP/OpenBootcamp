8f614a09e15a1b21634d9c982015520b06db8ec4
JuanRAlvarezP <95868133+JuanRAlvarezP@users.noreply.github.com>
2022-08-26 12:43:03 +0200

    Create Ejercicio4.js

---
+++ JavaScript/04. Cadenas de texto/Ejercicio4.js
@@ -0,0 +1,11 @@
+let nombre = "Juan";
+let apellido = "Alvarez";
+
+let estudiante = nombre.concat(" Alvarez")
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+let str_numer = estudiante.length
+let first_letter = estudiante[0]
+let last_letter = estudiante[estudiante.length - 1]
+let no_space = estudiante.replace(" ", "")
+let included = estudiante.includes(nombre)
