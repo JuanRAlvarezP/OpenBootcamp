57b67bf1094715e555412678c66f0e7d3e51b24a
JuanRAlvarezP <95868133+JuanRAlvarezP@users.noreply.github.com>
2022-08-29 12:10:41 +0200

    Create Ejercicio5.js

---
+++ JavaScript/05. Numeros en JavaScript/Ejercicio5.js
@@ -0,0 +1,14 @@
+let altura_cm = 173;
+let altura_m = 1.73;
+let peso_kg = 57.6;
+
+let altura_round_m = Math.round(altura_m)
+console.log(altura_round_m)
+
+let peso_round_kg = parseInt(peso_kg)
+console.log(peso_round_kg)
+
+let max_value_JS = Number.MAX_VALUE
+if (max_value_JS === max_value_JS +1) {
+    console.log("El maximo valor que se puede obtener en JavaScript mas uno es igual al valor maximo que se puede obtener en JavaScript")
+}
