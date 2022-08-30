f2b801ce5772b5e21a819d7c9727ca873e5ab03e
JuanRAlvarezP <95868133+JuanRAlvarezP@users.noreply.github.com>
2022-08-30 12:34:58 +0200

    Create conjuntos.js

---
+++ JavaScript/07. Trabajando con set y objetos/conjuntos.js
@@ -0,0 +1,11 @@
+const familia = new Set(["Guillermo", "Victoria", "Sebastian", "Miguel", "Ricardo", "Daniel"])
+
+console.log(familia)
+
+familia.add("Ricardo")
+
+console.log(familia)
+
+familia.add("Javascript")
+
+console.log(familia)
